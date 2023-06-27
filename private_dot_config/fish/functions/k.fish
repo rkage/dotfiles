8f22bcf4237d72b5465b4198e17ca65c8f128953
Nick M <4718+rkage@users.noreply.github.com>
2023-06-27 15:46:23 -0400

    update for new laptop arriving soon

--- private_dot_config/fish/functions/k.fish
+++ private_dot_config/fish/functions/k.fish
@@ -5,21 +5,21 @@ if command -qs kubectl
         kubectl $argv
     end
 
-    abbr -a -U sk 'kubectl -n kube-system'
-    abbr -a -U kg 'kubectl get'
-    abbr -a -U kgp 'kubectl get po'
-    abbr -a -U kga 'kubectl get --all-namespaces'
-    abbr -a -U kd 'kubectl describe'
-    abbr -a -U kdp 'kubectl describe po'
-    abbr -a -U krm 'kubectl delete'
-    abbr -a -U ktn 'kubectl top nodes --use-protocol-buffers'
-    abbr -a -U ke 'kubectl edit'
-    abbr -a -U kex 'kubectl exec -it'
-    abbr -a -U knrunning 'kubectl get pods --field-selector=status.phase!=Running'
-    abbr -a -U kfails 'kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
-    abbr -a -U kimg "kubectl get deployment --output=jsonpath='{.spec.template.spec.containers[*].image}'"
-    abbr -a -U kvs "kubectl view-secret"
-    abbr -a -U kgno 'kubectl get no --sort-by=.metadata.name -o wide'
-    abbr -a -U kdrain 'kubectl drain --ignore-daemonsets --delete-emptydir-data'
-    abbr -a -U kscale 'kubectl scale'
+    abbr -a sk 'kubectl -n kube-system'
+    abbr -a kg 'kubectl get'
+    abbr -a kgp 'kubectl get po'
+    abbr -a kga 'kubectl get --all-namespaces'
+    abbr -a kd 'kubectl describe'
+    abbr -a kdp 'kubectl describe po'
+    abbr -a krm 'kubectl delete'
+    abbr -a ktn 'kubectl top nodes --use-protocol-buffers'
+    abbr -a ke 'kubectl edit'
+    abbr -a kex 'kubectl exec -it'
+    abbr -a knrunning 'kubectl get pods --field-selector=status.phase!=Running'
+    abbr -a kfails 'kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
+    abbr -a kimg "kubectl get deployment --output=jsonpath='{.spec.template.spec.containers[*].image}'"
+    abbr -a kvs "kubectl view-secret"
+    abbr -a kgno 'kubectl get no --sort-by=.metadata.name -o wide'
+    abbr -a kdrain 'kubectl drain --ignore-daemonsets --delete-emptydir-data'
+    abbr -a kscale 'kubectl scale'
 end
