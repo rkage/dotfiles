dc86440dc0c5981f4e035f1ebb3bd0bc4c13ee1d
Nick M <4718+rkage@users.noreply.github.com>
2023-08-26 14:59:17 -0400

    few fixes here and there, add configure macos script

--- private_dot_config/fish/functions/k.fish
+++ private_dot_config/fish/functions/k.fish
@@ -4,22 +4,4 @@ if command -qs kubectl
     function k --wraps=kubectl --description="shorthand for kubectl"
         kubectl $argv
     end
-
-    abbr -a sk 'kubectl -n kube-system'
-    abbr -a kg 'kubectl get'
-    abbr -a kgp 'kubectl get po'
-    abbr -a kga 'kubectl get --all-namespaces'
-    abbr -a kd 'kubectl describe'
-    abbr -a kdp 'kubectl describe po'
-    abbr -a krm 'kubectl delete'
-    abbr -a ktn 'kubectl top nodes --use-protocol-buffers'
-    abbr -a ke 'kubectl edit'
-    abbr -a kex 'kubectl exec -it'
-    abbr -a knrunning 'kubectl get pods --field-selector=status.phase!=Running'
-    abbr -a kfails 'kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
-    abbr -a kimg "kubectl get deployment --output=jsonpath='{.spec.template.spec.containers[*].image}'"
-    abbr -a kvs "kubectl view-secret"
-    abbr -a kgno 'kubectl get no --sort-by=.metadata.name -o wide'
-    abbr -a kdrain 'kubectl drain --ignore-daemonsets --delete-emptydir-data'
-    abbr -a kscale 'kubectl scale'
 end
--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,19 +1,56 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 local config = {}
 
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {
+    position = {
+      x = 130,
+      y = 120,
+    }
+  })
+end)
+
 config.front_end = "WebGpu"
-config.initial_rows = 50
-config.initial_cols = 238
+config.initial_rows = 46
+config.initial_cols = 185
 
 config.font = wezterm.font("MonoLisa")
 config.font_size = 12
+config.harfbuzz_features = { "zero", "ss06", "ss07", "ss10", "ss11" }
 config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = "2px",
+  right = "2px",
+  top = "2px",
+  bottom = "2px"
+}
 
 config.color_scheme = "Nord (base16)"
 config.colors = {
   background = "#191c1f",
+  tab_bar = {
+    active_tab = {
+      bg_color = "#191c1f",
+      fg_color = "#d8dee9"
+    },
+    inactive_tab = {
+      bg_color = "#1f1f1f",
+      fg_color = "#808080"
+    },
+    inactive_tab_edge = "#1f1f1f"
+  }
+}
+
+config.window_frame = {
+  font = wezterm.font({
+    family = "MonoLisa",
+    weight = "Light"
+  }),
+  font_size = 11,
+  active_titlebar_bg = "#161616"
 }
 
 return config
